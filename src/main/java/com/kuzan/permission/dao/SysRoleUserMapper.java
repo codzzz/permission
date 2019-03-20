bda4f28b2737ffa96f05a16bdcd3aed86d95efd8
fengyajun <fengyajun@cxxmobi.com>
2019-03-20 17:27:02 +0800

    MGB 自动生成entity和dao代码

---
+++ src/main/java/com/kuzan/permission/dao/SysRoleUserMapper.java
@@ -0,0 +1,17 @@
+package com.kuzan.permission.dao;
+
+import com.kuzan.permission.entity.SysRoleUser;
+
+public interface SysRoleUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysRoleUser record);
+
+    int insertSelective(SysRoleUser record);
+
+    SysRoleUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysRoleUser record);
+
+    int updateByPrimaryKey(SysRoleUser record);
+}
---
+++ src/main/java/com/kuzan/permission/dao/SysUserMapper.java
@@ -0,0 +1,17 @@
+package com.kuzan.permission.dao;
+
+import com.kuzan.permission.entity.SysUser;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    int insertSelective(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysUser record);
+
+    int updateByPrimaryKey(SysUser record);
+}
